449dde972416df45eb9ea6f866810b30cd6c1c4a
erayushs <anmol8960@gmail.com>
2024-03-21 18:32:53 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”  (Crio.Do)

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,7 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = { backendEndpoint: "http://13.200.222.218:8082" };
 
 export default config;
+
+
+
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -16,13 +16,35 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try { 
+  const response = await fetch(config.backendEndpoint + "/cities");
+  const cities = await response.json();
+  return cities;
+  }
+  catch(err){
+  return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  let card = document.getElementById("data");
+  let content = document.createElement("div");
+  content.className = "col-6 col-md-4 col-lg-3 mb-4";
+  content.innerHTML =  `
+  <a href="pages/adventures/?city=${id}" id="${id}">
+    <div class="tile">
+      <div class="tile-text text-center">
+        <h5>${city}</h5>
+        <p>${description}</p>
+      </div>
+    <img class="img-responsive" src="${image}">
+    </div>
+  </a>
+  `;
+    card.appendChild(content); 
 
 }
 
